38b3e43de7b6b137da60cc05c1a0a159f3f96c54
doogerk <doogerk@gmail.com>
2017-12-18 11:50:15 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,14 +25,14 @@ classifiers = ['Development Status :: 4 - Beta',
                'Topic :: Software Development',
                'Topic :: System :: Hardware']
 
-setup(name              = 'Adafruit_HX3857',
+setup(name              = 'Adafruit_HX8357',
       version           = '1.0.0',
       author            = 'Tony DiCola with modifications by Doug Krafjack',
       author_email      = 'tdicola@adafruit.com',
-      description       = 'Library to control an HX3857 TFT LCD display.',
+      description       = 'Library to control an HX8357 TFT LCD display.',
       license           = 'MIT',
       classifiers       = classifiers,
-      url               = 'https://github.com/doogerk/Adafruit_HX3857_Python/',
+      url               = 'https://github.com/doogerk/Adafruit_HX8357_Python/',
       dependency_links  = ['https://github.com/adafruit/Adafruit_Python_GPIO/tarball/master#egg=Adafruit-GPIO-0.6.5'],
       install_requires  = ['Adafruit-GPIO>=0.6.5'],
       packages          = find_packages())
